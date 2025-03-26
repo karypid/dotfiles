67f915fa9c28cddd9c679a508d842671322071ad
Alexandros Karypidis <1221101+karypid@users.noreply.github.com>
2025-03-26 18:09:40 +0000

    Add environment scripts folder and install zsh themes

--- sh.nix
+++ sh.nix
@@ -5,12 +5,23 @@ let
     ll = "ls -l";
     fastfetch = "/usr/libexec/ublue-fastfetch";
   };
+  sourceShD = "
+    source ~/.sh.d/*
+  ";
 in
 {
+  home.file = {
+    ".sh.d" = {
+      source = ./sh.d;
+      recursive = true;
+    };
+  };
+
   programs.bash = {
     shellAliases = myAliases;
     enable = true;
     enableCompletion = true;
+    bashrcExtra = sourceShD;
   };
 
   programs.zsh = {
@@ -19,15 +30,22 @@ in
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true; 
+    initExtra = sourceShD;
 
-    initExtra = ''
-    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
-     %F{green}→%f "
-    RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
-    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
-    bindkey '^P' history-beginning-search-backward
-    bindkey '^N' history-beginning-search-forward
-    '';
+#    initExtra = ''
+#    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
+#     %F{green}→%f "
+#    RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
+#    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+#    bindkey '^P' history-beginning-search-backward
+#    bindkey '^N' history-beginning-search-forward
+#    '';
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "agnoster";
+      # theme = "robbyrussell";
+    };
   };
 
   programs.direnv = {
